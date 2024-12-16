e497618b27c7048d1fa9e2dedf078cc05d5f9b75
Christian Sinzinger <sinzi@sinzi.xyz>
2024-12-16 10:51:50 +0100

    New: Day 16

---
+++ day_16/day_16_Reindeer_Maze.py
@@ -0,0 +1,345 @@
+"""
+--- Day 15: Warehouse Woes ---
+You appear back inside your own mini submarine! Each Historian drives their mini submarine in a different direction; maybe the Chief has his own submarine down here somewhere as well?
+
+You look up to see a vast school of lanternfish swimming past you. On closer inspection, they seem quite anxious, so you drive your mini submarine over to see if you can help.
+
+Because lanternfish populations grow rapidly, they need a lot of food, and that food needs to be stored somewhere. That's why these lanternfish have built elaborate warehouse complexes operated by robots!
+
+These lanternfish seem so anxious because they have lost control of the robot that operates one of their most important warehouses! It is currently running amok, pushing around boxes in the warehouse with no regard for lanternfish logistics or lanternfish inventory management strategies.
+
+Right now, none of the lanternfish are brave enough to swim up to an unpredictable robot so they could shut it off. However, if you could anticipate the robot's movements, maybe they could find a safe option.
+
+The lanternfish already have a map of the warehouse and a list of movements the robot will attempt to make (your puzzle input). The problem is that the movements will sometimes fail as boxes are shifted around, making the actual movements of the robot difficult to predict.
+
+For example:
+
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+As the robot (@) attempts to move, if there are any boxes (O) in the way, the robot will also attempt to push those boxes. However, if this action would cause the robot or a box to move into a wall (#), nothing moves instead, including the robot. The initial positions of these are shown on the map at the top of the document the lanternfish gave you.
+
+The rest of the document describes the moves (^ for up, v for down, < for left, > for right) that the robot will attempt to make, in order. (The moves form a single giant sequence; they are broken into multiple lines just to make copy-pasting easier. Newlines within the move sequence should be ignored.)
+
+Here is a smaller example to get started:
+
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+Were the robot to attempt the given sequence of moves, it would push around the boxes as follows:
+
+Initial state:
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move <:
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move ^:
+########
+#.@O.O.#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move ^:
+########
+#.@O.O.#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move >:
+########
+#..@OO.#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move >:
+########
+#...@OO#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move >:
+########
+#...@OO#
+##..O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+Move v:
+########
+#....OO#
+##..@..#
+#...O..#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move v:
+########
+#....OO#
+##..@..#
+#...O..#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move <:
+########
+#....OO#
+##.@...#
+#...O..#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move v:
+########
+#....OO#
+##.....#
+#..@O..#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move >:
+########
+#....OO#
+##.....#
+#...@O.#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move >:
+########
+#....OO#
+##.....#
+#....@O#
+#.#.O..#
+#...O..#
+#...O..#
+########
+
+Move v:
+########
+#....OO#
+##.....#
+#.....O#
+#.#.O@.#
+#...O..#
+#...O..#
+########
+
+Move <:
+########
+#....OO#
+##.....#
+#.....O#
+#.#O@..#
+#...O..#
+#...O..#
+########
+
+Move <:
+########
+#....OO#
+##.....#
+#.....O#
+#.#O@..#
+#...O..#
+#...O..#
+########
+The larger example has many more moves; after the robot has finished those moves, the warehouse would look like this:
+
+##########
+#.O.O.OOO#
+#........#
+#OO......#
+#OO@.....#
+#O#.....O#
+#O.....OO#
+#O.....OO#
+#OO....OO#
+##########
+The lanternfish use their own custom Goods Positioning System (GPS for short) to track the locations of the boxes. The GPS coordinate of a box is equal to 100 times its distance from the top edge of the map plus its distance from the left edge of the map. (This process does not stop at wall tiles; measure all the way to the edges of the map.)
+
+So, the box shown below has a distance of 1 from the top edge of the map and 4 from the left edge of the map, resulting in a GPS coordinate of 100 * 1 + 4 = 104.
+
+#######
+#...O..
+#......
+The lanternfish would like to know the sum of all boxes' GPS coordinates after the robot finishes moving. In the larger example, the sum of all boxes' GPS coordinates is 10092. In the smaller example, the sum is 2028.
+
+Predict the motion of the robot and boxes in the warehouse. After the robot is finished moving, what is the sum of all boxes' GPS coordinates?
+"""
+
+import networkx as nx
+
+
+def parse_input(file_path):
+    """
+    Reads the maze input from a file and returns the lines as a list of strings.
+    """
+    with open(file_path, "r") as f:
+        return f.read().strip().split("\n")
+
+
+def build_graph(lines):
+    """
+    Builds a directed graph (DiGraph) from the maze input.
+
+    Parameters:
+        lines (list of str): The maze input as a list of strings.
+
+    Returns:
+        nx.DiGraph: The graph representing the maze.
+        tuple: The start node (position and initial direction).
+    """
+    graph = nx.DiGraph()
+    directions = (1, -1, 1j, -1j)  # right, left, down, up
+    start, end = None, None
+
+    # Parse the maze and add nodes
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            if char == "#":
+                continue  # Skip walls
+            position = i + 1j * j
+            if char == "S":
+                start = (position, 1j)  # Start node with initial direction (down)
+            if char == "E":
+                end = position  # End position
+            for direction in directions:
+                graph.add_node((position, direction))
+
+    # Add edges for movements and rotations
+    for position, direction in graph.nodes:
+        # Movement edges (forward)
+        next_position = position + direction
+        if (next_position, direction) in graph.nodes:
+            graph.add_edge((position, direction), (next_position, direction), weight=1)
+
+        # Rotation edges (90 degrees left or right)
+        for rotation in (-1j, 1j):
+            rotated_direction = direction * rotation
+            graph.add_edge((position, direction), (position, rotated_direction), weight=1000)
+
+    # Add edges from end position to "end" node
+    for direction in directions:
+        graph.add_edge((end, direction), "end", weight=0)
+
+    return graph, start
+
+
+def solve_part_1(graph, start):
+    """
+    Solves Part 1: Finds the shortest path length (cost) from start to end.
+
+    Parameters:
+        graph (nx.DiGraph): The graph representing the maze.
+        start (tuple): The start node (position and initial direction).
+
+    Returns:
+        int: The cost of the shortest path.
+    """
+    return nx.shortest_path_length(graph, start, "end", weight="weight")
+
+
+def solve_part_2(graph, start):
+    """
+    Solves Part 2: Finds the number of unique positions visited across all shortest paths.
+
+    Parameters:
+        graph (nx.DiGraph): The graph representing the maze.
+        start (tuple): The start node (position and initial direction).
+
+    Returns:
+        int: The count of unique positions visited across all shortest paths.
+    """
+    all_paths = nx.all_shortest_paths(graph, start, "end", weight="weight")
+    unique_positions = {
+        position
+        for path in all_paths
+        for position, _ in path[:-1]  # Exclude the final "end" node
+    }
+    return len(unique_positions)
+
+
+def main():
+    maze = [line.strip() for line in open("./day_16/day_16_input.txt", "r").readlines()]
+    graph, start = build_graph(maze)
+
+    # Solve Part 1 and Part 2
+    part_1_result = solve_part_1(graph, start)
+    part_2_result = solve_part_2(graph, start)
+
+    # Print results
+    print(f"Part 1: Shortest path cost = {part_1_result}")
+    print(f"Part 2: Unique positions visited = {part_2_result}")
+
+
+if __name__ == "__main__":
+    main()
